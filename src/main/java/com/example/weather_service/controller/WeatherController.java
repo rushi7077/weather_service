450e6f62b01f42f875c69a121402dcb0839354b1
Rushi Baravkar <rushibaravkar1012@gmail.com>
2026-01-05 18:21:52 +0530

    updated

--- src/main/java/com/example/weather_service/controller/WeatherController.java
+++ src/main/java/com/example/weather_service/controller/WeatherController.java
@@ -5,6 +5,7 @@ import com.example.weather_service.repository.WeatherRepository;
 import com.example.weather_service.service.CacheInspectionService;
 import com.example.weather_service.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,11 +24,13 @@ public class WeatherController {
     CacheInspectionService cacheInspectionService;
 
     @GetMapping
+    @PreAuthorize("hasAuthority('WEATHER_READ')")
     public String getWeather(@RequestParam String city){
         return weatherService.getWeatherByCity(city);
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('WEATHER_WRITE')")
     public Weather addWeather(@RequestBody Weather weather){
         return weatherRepository.save(weather);
     }
@@ -47,6 +50,7 @@ public class WeatherController {
         return weatherService.updateWeather(city, weatherUpdate);
     }
     @DeleteMapping("/{city}")
+    @PreAuthorize("hasAuthority('WEATHER_DELETE')")
     public String deleteWeather(@PathVariable String city) {
         weatherService.deleteWeather(city);
         return "Weather data for " + city + " has been deleted and cache evicted.";

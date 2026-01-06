52b9e28562acb8280276957c097d423c35f8ee7a
Rushi Baravkar <rushibaravkar1012@gmail.com>
2026-01-06 13:18:30 +0530

    updated

---
+++ src/main/java/com/example/weather_service/entity/UserResponse.java
@@ -0,0 +1,37 @@
+package com.example.weather_service.entity;
+
+public class UserResponse {
+    private long id;
+    private  String username;
+    private String role;
+
+    public UserResponse(long id, String username, String role) {
+        this.id = id;
+        this.username = username;
+        this.role = role;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}

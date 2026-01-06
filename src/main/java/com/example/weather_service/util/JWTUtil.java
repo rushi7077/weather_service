d5856e4c162af5ef717750cdc0ba0e54d7536b15
Rushi Baravkar <rushibaravkar1012@gmail.com>
2026-01-06 18:57:06 +0530

    updated

--- src/main/java/com/example/weather_service/util/JWTUtil.java
+++ src/main/java/com/example/weather_service/util/JWTUtil.java
@@ -4,8 +4,11 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+
 
 import javax.crypto.SecretKey;
 import java.util.Date;
@@ -13,42 +16,46 @@ import java.util.Date;
 @Component
 public class JWTUtil {
 
-    private final long EXPIRATION_TIME = (1000*60*60);
-    private final String SECRET = "this-is-my-super-secret-key-54789369738464";
-    private final SecretKey KEY = Keys.hmacShaKeyFor(SECRET.getBytes());
+    @Value("${jwt.expiration_time}")
+    private long expirationTime;  // Injected from properties
+
+    @Value("${jwt.secret}")
+    private String secret;        // Injected from properties
+
+    private SecretKey key;
+
+    @PostConstruct
+    public void init() {
+    this.key =Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
 
     public String generateToken(String username){
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis()+EXPIRATION_TIME))
-                .signWith(KEY, SignatureAlgorithm.HS256)
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
-
-
     }
+
     public String extractUserName(String token){
         return extractClaims(token).getSubject();
-
-
     }
 
     private Claims extractClaims(String token) {
         return Jwts.parserBuilder()
-                .setSigningKey(KEY)
+                .setSigningKey(key)
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
     }
 
     public boolean validateToken(UserDetails userDetails, String username, String token) {
-
-        return username.equals(userDetails.getUsername()) && ! isTokenExpired(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
     }
 
     private boolean isTokenExpired(String token) {
         return extractClaims(token).getExpiration().before(new Date());
     }
-
-
 }
